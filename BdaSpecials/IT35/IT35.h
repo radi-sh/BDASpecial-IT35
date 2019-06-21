efca09919e292b53956a3a233fade59e5cbd7b95
radi_sh <masamory7@gmail.com>
2019-06-21 20:36:39 +0900

    [IT35] IBdaSpecials2の変更に対応

--- BdaSpecials/IT35/IT35.h
+++ BdaSpecials/IT35/IT35.h
@@ -2,7 +2,7 @@
 
 #include "IBdaSpecials2.h"
 
-class CIT35Specials : public IBdaSpecials2b2
+class CIT35Specials : public IBdaSpecials2b3
 {
 public:
 	CIT35Specials(HMODULE hMySelf, CComPtr<IBaseFilter> pTunerDevice);
